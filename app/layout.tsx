d68dfdf38b9e01aaa72c17ef1273c69b08921159
KevinC76 <xckevinxc67@gmail.com>
2025-05-02 15:51:05 +0700

    fix: navbar bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="relative flex flex-col">
             <Navbar />
             <main className="flex-grow ">{children}</main>
             <Footer />
