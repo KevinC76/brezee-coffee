3a71611958e3dd452267e1e61b865e042cd60d96
KevinC76 <xckevinxc67@gmail.com>
2025-05-08 12:04:49 +0700

    feat: add animation for cards

--- components/reusable/cards.tsx
+++ components/reusable/cards.tsx
@@ -4,6 +4,8 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 
+import { motion, AnimatePresence } from 'framer-motion';
+
 type CardProps = {
   image_path: string;
   name: string;
@@ -13,19 +15,34 @@ export default function Cards({ image_path, name }: CardProps) {
   const [overlay, setOverlay] = useState(false);
 
   return (
-    <div
+    <motion.div
       className="relative w-[250px] h-[250px]"
-      onMouseMove={() => setOverlay(true)}
-      onMouseLeave={() => setOverlay(false)}
+      onHoverStart={() => setOverlay(true)}
+      onHoverEnd={() => setOverlay(false)}
     >
-      {overlay && (
-        <div className="absolute inset-0 z-10 flex  items-center justify-center transition delay-150 duration-300 ease-in-out">
-          <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full flex items-center justify-center rounded-md"></div>
-          <h1 className="z-10 px-3 py-1 text-white opacity-100 bg-brezee_primary rounded-full">
-            <span>{name}</span>
-          </h1>
-        </div>
-      )}
+      <AnimatePresence>
+        {overlay && (
+          <motion.div
+            className="absolute inset-0 z-10 flex  items-center justify-center transition delay-150 duration-300 ease-in-out"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            key={name}
+            transition={{ duration: 0.3 }}
+          >
+            <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full flex items-center justify-center rounded-md"></div>
+            <motion.h1
+              className="z-10 px-3 py-1 text-white opacity-100 bg-brezee_primary rounded-full"
+              initial={{ y: 10 }}
+              animate={{ y: 0 }}
+              exit={{ y: 10 }}
+              key={name}
+            >
+              <span>{name}</span>
+            </motion.h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       <Image
         src={image_path}
@@ -33,6 +50,6 @@ export default function Cards({ image_path, name }: CardProps) {
         fill
         style={{ objectFit: 'cover', borderRadius: '4px' }}
       />
-    </div>
+    </motion.div>
   );
 }
